6275a4a418489a33302f57c0b3c60fa20402652c
SSEELLLLEERRSSDD <116637072+SSEELLLLEERRSSDD@users.noreply.github.com>
2023-06-13 13:50:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,191 +2,167 @@ import telebot
 import sqlite3
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 from telebot import apihelper, types
 
-API_KEY=os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
-adminid= int(os.getenv("ADMIN_ID"))
-password=os.getenv("PASSSWORD")
+adminid = int(os.getenv("ADMIN_ID"))
+password = os.getenv("PASSSWORD")
+conn = sqlite3.connect('order.db', check_same_thread=False)
+
+
+# ЗАПОЛНЕНИЕ БД dishes
+def add_db_dishes(namedish, price):
+    db_data = [(namedish, price)]
+    conn.executemany("INSERT INTO dishes(namedish,price) VALUES (?, ?)", db_data)  # Запись данных в БД
+    conn.commit()  # Сохранение данных в БД
+
+
+# ЗАПОЛНЕНИЕ БД user
+def add_db_user(persondiscount, amountoforders, preferreddish):
+    db_data = [(persondiscount, amountoforders, preferreddish)]
+    conn.executemany("INSERT INTO user(persondiscount,amountoforders,preferreddish) VALUES (?, ?,?)", db_data)
+    conn.commit()
+
+
+# ЗАПОЛНЕНИЕ БД orders
+def add_db_orders(countdish, suminrubles, timeorder):
+    db_data = [(countdish, suminrubles, timeorder)]
+    conn.executemany("INSERT INTO orders(countdish,suminrubles,timeorder) VALUES ( ?,?,?)", db_data)
+    conn.commit()
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
-    if user_id ==  adminid:
+    if user_id == adminid:
+
         markup = types.InlineKeyboardMarkup()
-        menu=types.InlineKeyboardButton('Меню',callback_data='menu')
+        menu = types.InlineKeyboardButton('Меню', callback_data='menu')
         markup.add(menu)
-        bot.send_message(message.chat.id,'Вы успешно вошли как администратор',reply_markup=markup)
+        bot.send_message(message.chat.id, 'Вы успешно вошли как администратор', reply_markup=markup)
 
     else:
         bot.send_message(message.chat.id, 'Вы успешно вошли как пользователь')
 
 
-
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
-    if callback.data=='menu':
-        markup2=types.InlineKeyboardMarkup()
+    if callback.data == 'menu':
+        markup2 = types.InlineKeyboardMarkup()
         adddate = types.InlineKeyboardButton('Добавить данные', callback_data='add')
         outostdate = types.InlineKeyboardButton('Вывести данные', callback_data='outpost')
-        markup2.add(adddate,outostdate)
-        bot.send_message(callback.message.chat.id,'Вы успешно зашли в меню',reply_markup=markup2)
-    if callback.data=='add':
+        markup2.add(adddate, outostdate)
+        bot.send_message(callback.message.chat.id, 'Вы успешно зашли в меню', reply_markup=markup2)
 
+    if callback.data == 'add':
         markup3 = types.InlineKeyboardMarkup()
         dishesbtn = types.InlineKeyboardButton('dishes', callback_data='dishesadd')
         ordersbtn = types.InlineKeyboardButton('orders', callback_data='ordersadd')
         userbtn = types.InlineKeyboardButton('user', callback_data='useradd')
         markup3.add(dishesbtn, ordersbtn, userbtn)
         bot.send_message(callback.message.chat.id, 'Выберите бд', reply_markup=markup3)
-    if callback.data=='outpost':
 
+    if callback.data == 'outpost':
         markup4 = types.InlineKeyboardMarkup()
         dishesbtn = types.InlineKeyboardButton('dishes', callback_data='dishesout')
         ordersbtn = types.InlineKeyboardButton('orders', callback_data='ordersout')
         userbtn = types.InlineKeyboardButton('user', callback_data='userout')
         markup4.add(dishesbtn, ordersbtn, userbtn)
         bot.send_message(callback.message.chat.id, 'Выберите бд', reply_markup=markup4)
-    #ЗАПОЛНЕНИЕ БД DISHES
+
+    # ЗАПОЛНЕНИЕ БД DISHES
     if callback.data == 'dishesadd':
         data = bot.send_message(callback.message.chat.id, 'Введите название блюда, цену')
 
-        def create_db(namedish,price):
-            conn = sqlite3.connect('order.db')
-
-            db_data = [(namedish,price)]
-            conn.executemany("INSERT INTO dishes(namedish,price) VALUES (?, ?)", db_data)  # Запись данных в БД
-
-            conn.commit()  # Сохранение данных в БД
-            conn.close()
         @bot.message_handler()
-        def filling_db_2(message):
+        def filling_db_dishes(message):
             text = message.text.split(',')
             namedish = text[0]
             price = text[1]
+            add_db_dishes(namedish, price)
 
-            create_db(namedish, price)
-
-        bot.register_next_step_handler(data, filling_db_2)
+        bot.register_next_step_handler(data, filling_db_dishes)
 
     # ЗАПОЛНЕНИЕ БД orders
     if callback.data == 'ordersadd':
         data = bot.send_message(callback.message.chat.id, 'Введите колво, сумму в рублях, время заказа')
 
-        def create_db( countdish,suminrubles,  timeorder):
-            conn = sqlite3.connect('order.db')
-
-            db_data = [( countdish,suminrubles,  timeorder)]
-            conn.executemany("INSERT INTO orders(countdish,suminrubles,timeorder) VALUES ( ?,?,?)", db_data)  # Запись данных в БД
-
-            conn.commit()  # Сохранение данных в БД
-            conn.close()
         @bot.message_handler()
-        def filling_db_2(message):
+        def filling_db_orders(message):
             text = message.text.split(',')
 
             countdish = text[0]
             suminrubles = text[1]
             timeorder = text[2]
 
-            create_db( countdish,suminrubles,  timeorder)
+            add_db_orders(countdish, suminrubles, timeorder)
 
-        bot.register_next_step_handler(data, filling_db_2)
+        bot.register_next_step_handler(data, filling_db_orders)
 
-# ЗАПОЛНЕНИЕ БД user
+    # ЗАПОЛНЕНИЕ БД user
+    if callback.data == 'useradd':
+        data = bot.send_message(callback.message.chat.id, 'Введите персональную скидку, колвозаказов, любимое блюдо')
 
-        def create_db(persondiscount, amountoforders,preferreddish):
-            conn = sqlite3.connect('order.db')
-
-            db_data = [(persondiscount, amountoforders,preferreddish)]
-            conn.executemany("INSERT INTO user(persondiscount,amountoforders,preferreddish) VALUES (?, ?,?)", db_data)  # Запись данных в БД
-
-            conn.commit()  # Сохранение данных в БД
-            conn.close()
         @bot.message_handler()
-        def filling_db_2(message):
+        def filling_db_user(message):
             text = message.text.split(',')
             persondiscount = text[0]
             amountoforders = text[1]
             preferreddish = text[2]
 
-            create_db(persondiscount, amountoforders,preferreddish)
+            add_db_user(persondiscount, amountoforders, preferreddish)
 
-        bot.register_next_step_handler(data, filling_db_2)
+        bot.register_next_step_handler(data, filling_db_user)
 
-
-# ВЫВОДДАННЫХ bd dishes
+    # ВЫВОДДАННЫХ bd dishes
 
     if callback.data == 'dishesout':
-        data = bot.send_message(callback.message.chat.id, 'Вывожу список данных bd dishes')
-        try:
-            sqlite_connection = sqlite3.connect('order.db')
-            conn = sqlite_connection.cursor()
-            sqlite_select_query = """SELECT * from dishes"""
-            conn.execute(sqlite_select_query)
-            records = conn.fetchall()
-            bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
-            i = 1
-            for row in records:
-                bot.send_message(callback.message.chat.id, 'ID:' +str(i)+';namedish: '+ row[0]+';price:'+row[1]+'\n\n')
-                i+=1
-
-            conn.close()
-        except sqlite3.Error as error:
-            bot.send_message(callback.message.chat.id,"Ошибка при работе с SQLite:"+ error)
-        finally:
-            if sqlite_connection:
-                sqlite_connection.close()
-                bot.send_message(callback.message.chat.id,"Соединение с SQLite закрыто")
+        bot.send_message(callback.message.chat.id, 'Вывожу список данных bd dishes')
+        cos = conn.cursor()
+        sqlite_select_query = """SELECT * from orders"""
+        cos.execute(sqlite_select_query)
+        records = cos.fetchall()
+        bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
+        i = 1
+        for row in records:
+            bot.send_message(callback.message.chat.id,
+                             'ID:' + str(i) + ';namedish: ' + row[0] + ';price:' + row[1] + '\n\n')
+            i += 1
 
     # ВЫВОДДАННЫХ bd orders
 
     if callback.data == 'ordersout':
-        data = bot.send_message(callback.message.chat.id, 'Вывожу список данных bd orders')
-        try:
-            sqlite_connection = sqlite3.connect('order.db')
-            conn = sqlite_connection.cursor()
-            sqlite_select_query = """SELECT * from orders"""
-            conn.execute(sqlite_select_query)
-            records = conn.fetchall()
-            bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
-            i = 1
-            for row in records:
-                bot.send_message(callback.message.chat.id, 'ID:' +str(i)+';countdish: '+ row[0]+';suminrubles:'+row[1]+';timeorder: '+ row[2]+'\n\n')
-                i+=1
+        bot.send_message(callback.message.chat.id, 'Вывожу список данных bd orders')
+        cos = conn.cursor()
+        sqlite_select_query = """SELECT * from orders"""
+        cos.execute(sqlite_select_query)
+        records = cos.fetchall()
+        bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
+        i = 1
+        for row in records:
+            bot.send_message(callback.message.chat.id,
+                             'ID:' + str(i) + ';countdish: ' + row[0] + ';suminrubles:' + row[1] + ';timeorder: ' + row[
+                                 2] + '\n\n')
+            i += 1
 
-            conn.close()
-        except sqlite3.Error as error:
-            bot.send_message(callback.message.chat.id,"Ошибка при работе с SQLite:"+ error)
-        finally:
-            if sqlite_connection:
-                sqlite_connection.close()
-                bot.send_message(callback.message.chat.id,"Соединение с SQLite закрыто")
-
-# ВЫВОДДАННЫХ bd user
+    # ВЫВОДДАННЫХ bd user
 
     if callback.data == 'userout':
-        data = bot.send_message(callback.message.chat.id, 'Вывожу список данных bd user')
-        try:
-            sqlite_connection = sqlite3.connect('order.db')
-            conn = sqlite_connection.cursor()
-            sqlite_select_query = """SELECT * from user"""
-            conn.execute(sqlite_select_query)
-            records = conn.fetchall()
-            bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
-            i = 1
-            for row in records:
-                bot.send_message(callback.message.chat.id, 'ID:' +str(i)+';persondiscount: '+ row[0]+';amountoforders:'+row[1]+';preferreddish: '+ row[2]+'\n\n')
-                i+=1
-
-            conn.close()
-        except sqlite3.Error as error:
-            bot.send_message(callback.message.chat.id,"Ошибка при работе с SQLite:"+ error)
-        finally:
-            if sqlite_connection:
-                sqlite_connection.close()
-                bot.send_message(callback.message.chat.id,"Соединение с SQLite закрыто")
+        bot.send_message(callback.message.chat.id, 'Вывожу список данных bd user')
+        cos = conn.cursor()
+        sqlite_select_query = """SELECT * from orders"""
+        cos.execute(sqlite_select_query)
+        records = cos.fetchall()
+        bot.send_message(callback.message.chat.id, 'Всепго строк: ' + str(len(records)))
+        i = 1
+        for row in records:
+            bot.send_message(callback.message.chat.id,
+                             'ID:' + str(i) + ';persondiscount: ' + row[0] + ';amountoforders:' + row[
+                                 1] + ';preferreddish: ' + row[2] + '\n\n')
+            i += 1
 
 
 bot.polling(none_stop=True)
